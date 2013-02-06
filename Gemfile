f3ef3b1e00de9de5c25df3c553e7abcd0548a9fb
Henrik Berggren <henrik@henrikberggren.com>
2013-02-06 13:04:02 +0100

    init

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+gem 'sinatra'
+gem 'data_mapper'
+gem 'sinatra-contrib'
+
+group :development do
+  gem 'mysql2'
+  gem 'dm-mysql-adapter'
+  gem 'shotgun'
+end
+
+group :production do
+  gem 'pg'
+  gem 'dm-postgres-adapter'
+end
---
+++ app.rb
@@ -0,0 +1,94 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'data_mapper'
+require "sinatra/cookies"
+
+class Book
+  include DataMapper::Resource
+  property :id, Serial 
+  property :title, String
+  property :author, String
+  property :cover_url, Text
+end
+
+class Quote
+  include DataMapper::Resource
+  property :id, Serial 
+  property :quote, Text
+  property :username, String
+
+  belongs_to :book
+end
+
+class User
+  include DataMapper::Resource
+  property :id, Serial 
+  property :score, Text
+  property :avatar_url, Text
+end
+
+DataMapper.finalize
+DataMapper::Logger.new($stdout, :debug)
+
+configure do
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] || {
+    :adapter  => 'mysql',
+    :host     => 'localhost',
+    :username => 'root' ,
+    :password => '',
+    :database => 'gameofquotes'}))
+  DataMapper.auto_upgrade!  
+end
+
+
+get '/' do
+  score = cookies[:gof_score]
+  @s = score
+  if score && !score.empty? && score.split(',').size == 11
+    redirect "/finish"
+  else
+    @quote = Quote.first(:offset => rand(3))
+    @win = @quote.book
+    @lose = Book.first(:id.not => @win.id, :offset => rand(2))
+
+    erb :quote
+  end
+end
+
+get '/r' do
+  score = cookies[:gof_score]
+  if score && !score.empty? && score.split(',').size == 11
+    redirect "/finish"
+  else
+    add_to_score(params[:r])
+    redirect "/"
+  end
+end
+
+get '/finish' do
+  erb :finish
+  score = cookies[:gof_score]
+  if score && !score.empty? && score.split(',').size == 11
+    @result = 0
+    score.split(',').each { |r| @result += r.to_i }
+    erb :finish
+  else
+    redirect "/"
+  end
+end
+
+get '/restart' do
+  cookies[:gof_score] = nil
+  redirect "/"
+end
+
+get('/static/:filename') { send_file("./static/#{params[:filename]}") }
+get('/fonts/:filename') { send_file("./fonts/#{params[:filename]}") }
+
+def add_to_score(result)
+  score = cookies[:gof_score]
+  score = "#{score},#{result}"
+  cookies[:gof_score] = score
+end
+
